3df1af1aa7a7ec1374df3269fe7bb7d4b2fae42f
Elise Gan <eliseganx@gmail.com>
2017-06-20 18:21:36 -0700

    Add files via upload

--- lab.js
+++ lab.js
@@ -1,8 +1,7 @@
-//Part 1
 function parse() {
   var xmlhttp = new XMLHttpRequest();
   var jsondata = "data.json";
-  // for part 3: var jsondata = "https://messagehub.herokuapp.com/messages.json";
+  //for Part 2: var jsondata = "https://messagehub.herokuapp.com/messages.json";
 
   xmlhttp.onreadystatechange = function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -21,7 +20,4 @@ function print(array) {
     output += array[i].content + " " + array[i].username + "<br>";
   }
   document.getElementById("messages").innerHTML = output;
-}
-
-// Part 2: The data doesn't load from a local machine. This makes sense because XMLHttpRequest requests a document from the web server, not the local machine. The document in this case is data.json
-// Part 3: The data doesn't load because the link given has a different origin that index.html 
+} 
